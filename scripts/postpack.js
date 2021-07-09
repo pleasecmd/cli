b98d182798561c8013c7f711f893b4c5c1d35269
Pouya <pouya.eghbali@yandex.com>
2021-07-09 18:49:30 +0200

    Added gzexe

--- scripts/postpack.js
+++ scripts/postpack.js
@@ -1,9 +1,13 @@
-const { renameSync, readdirSync } = require("fs");
-
+const { renameSync, rmSync, readdirSync } = require("fs");
+const { execSync } = require("child_process");
 const { version } = require("../package.json");
 
 readdirSync("./dist").forEach((file) => {
   const oldName = `./dist/${file}`;
   const newName = "./dist/" + "please-" + version + file.slice(3);
   renameSync(oldName, newName);
+  if (!file.endsWith(".exe")) {
+    execSync(`gzexe ${newName}`, { stdio: "ignore" });
+    rmSync(newName + "~");
+  }
 });
