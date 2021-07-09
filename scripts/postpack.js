802df12869b8e1d0fa1828e7f6323d728677b32a
Pouya <pouya.eghbali@yandex.com>
2021-07-09 19:05:40 +0200

    Remove gzexe

--- scripts/postpack.js
+++ scripts/postpack.js
@@ -1,13 +1,8 @@
-const { renameSync, rmSync, readdirSync } = require("fs");
-const { execSync } = require("child_process");
+const { renameSync, readdirSync } = require("fs");
 const { version } = require("../package.json");
 
 readdirSync("./dist").forEach((file) => {
   const oldName = `./dist/${file}`;
   const newName = "./dist/" + "please-" + version + file.slice(3);
   renameSync(oldName, newName);
-  if (!file.endsWith(".exe")) {
-    execSync(`gzexe ${newName}`, { stdio: "ignore" });
-    rmSync(newName + "~");
-  }
 });
